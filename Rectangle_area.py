940d8716383e69f2ba5900107670d9b9dc32e2a8
Desmonterrador <73971777+Desmonterrador@users.noreply.github.com>
2022-08-17 22:53:19 -0500

    Create Rectangle_area.py

---
+++ Rectangle_area.py
@@ -0,0 +1,6 @@
+print ("CALCULADORA PARA SACAR EL AREA DE UN RECTANGULO")
+base=float(input("Coloque el valor de la base: "))
+altura=float(input("coloque el valor de la altura: "))
+area=0
+area=(base*altura)
+print("El area del rectangulo es:",area,)
